a010ead39a224be8b41049932ecd64711251f180
Ankit Raj <125204924+ankit4456@users.noreply.github.com>
2025-05-17 17:38:44 +0530

    Add files via upload

---
+++ GSAP/gsap1/script.js
@@ -0,0 +1,29 @@
+gsap.to("#box1",{
+    x:2000,
+    duration:5,
+    delay:1,
+    rotate:360,
+    backgroundColor:"black",
+   borderRadius:"50%",
+   scale:12
+
+
+})
+gsap.from("#box2",{
+    x:2000,
+    duration:2,
+    delay:1.2,
+    borderRadius:"20%"
+    
+
+
+})
+
+
+gsap.to("#box3",{
+    x:1120,
+    duration:2,
+    delay:1.4,
+     borderRadius:"30%"
+
+})
