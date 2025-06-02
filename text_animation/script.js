d5c8b26b8fdab9917fb3541f3a11759712853f62
Ankit Raj <125204924+ankit4456@users.noreply.github.com>
2025-06-03 04:33:25 +0530

    Add files via upload

---
+++ text_animation/script.js
@@ -0,0 +1,37 @@
+function breaktheText() {
+    var h1 = document.querySelector("h1"); // h1 select
+    var hiText = h1.textContent;           // h1 text le raha
+    var splitText = hiText.split("");      // har character split
+    var clutter = "";
+    var halfValue = Math.floor(splitText.length / 2); // define half
+
+    splitText.forEach(function(elem, idx) {
+        if (idx < halfValue) {
+            clutter += `<span class="a">${elem}</span>`;
+        } else {
+            clutter += `<span class="b">${elem}</span>`;
+        }
+    });
+
+    h1.innerHTML = clutter; // final HTML inject
+}
+
+breaktheText();
+
+// Animation for first half
+gsap.from("h1 .a", {
+    y: 80,
+    duration: 0.6,
+    delay: 0.5,
+    opacity: 0,
+    stagger: 0.15
+});
+
+// Animation for second half
+gsap.from("h1 .b", {
+    y: 80,
+    duration: 0.6,
+    delay: 0.5,
+    opacity: 0,
+    stagger: -0.15
+});
